93f8240bbf0f42932738453553af5fbd45c7b78f
whyMUlose <sonthaihoathcs@gmail.com>
2023-10-30 20:57:42 +0700

    add text to speech

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.example.translatetest1 {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires jlayer;
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires java.logging;
+    requires jsapi;
 
     opens com.example.translatetest1 to javafx.fxml;
     exports com.example.translatetest1;
